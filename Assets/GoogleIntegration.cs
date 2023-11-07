bc17884096d54779448c26e371f600c41e15cdf6
Jeka107 <shmulevich165@gmail.com>
2023-11-08 00:16:29 +0200

    Google Play

---
+++ Assets/GoogleIntegration.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Threading.Tasks;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine;
+
+public class GoogleIntegration : MonoBehaviour
+{
+    public string GooglePlayToken;
+    public string GooglePlayError;
+
+    public async Task Authenticate()
+    {
+        PlayGamesPlatform.Activate();
+        await UnityServices.InitializeAsync();
+
+        PlayGamesPlatform.Instance.Authenticate((success) =>
+        {
+            if(success==SignInStatus.Success)
+            {
+                Debug.Log("Login with google was successful");
+                PlayGamesPlatform.Instance.RequestServerSideAccess(true, code =>
+                 {
+                     Debug.Log($"Auth code is{code}");
+                     GooglePlayToken = code;
+                 });
+            }
+            else
+            {
+                GooglePlayError = "Failed to retrieve GPG auth code";
+                Debug.LogError("Login Unseccessful");
+            }
+        });
+
+        await AuthenticateWithUnity();
+    }
+    private async Task AuthenticateWithUnity()
+    {
+        try
+        {
+            await AuthenticationService.Instance.SignInWithGoogleAsync(GooglePlayToken);
+        }
+        catch(AuthenticationException ex)
+        {
+            Debug.LogException(ex);
+            throw;
+        }
+        catch(RequestFailedException ex)
+        {
+            Debug.LogException(ex);
+            throw;
+        }
+    }
+}
